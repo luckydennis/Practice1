a786c297055bfdcb064a93cd1c5d4c05a4be2420
luckydennis <dennishan123@gmail.com>
2019-02-01 17:43:30 -0800

    test

--- Practice1/Practice1.cpp
+++ Practice1/Practice1.cpp
@@ -17,20 +17,20 @@
 //	else
 //		return n * factorial(n - 1);
 //}
-	int fib(int n)
-	{
-		if (n <= 0)
-			return 0;
-		else if (n == 1)
-			return 1;
-		return fib(n - 1) + fib(n - 2);
+int fib(int n)
+{
+	if (n <= 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	return fib(n - 1) + fib(n - 2);
 
-	}
+}
 void allFib(int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-			std::cout << i << ": " << fib(i);
+		std::cout << i << ": " << fib(i);
 	}
 }
 
